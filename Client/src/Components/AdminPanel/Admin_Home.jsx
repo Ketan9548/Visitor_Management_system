fcbb019f0c4de4f1509ceeea0790297bb30bdc7b
Ketan9548 <kc918062@gmail.com>
2025-02-20 11:03:20 +0530

    Done Changes

--- Client/src/Components/AdminPanel/Admin_Home.jsx
+++ Client/src/Components/AdminPanel/Admin_Home.jsx
@@ -1,13 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Menu, X } from "lucide-react"; // Icons for menu toggle
 
 const Admin_Home = () => {
+  const [isToggled, setIsToggled] = useState(true);
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center w-full max-w-md">
-        <p>This is Home Page</p>
+    <>
+      <div className="flex w-full h-screen">
+        {/* Sidebar */}
+        <div
+          className={`fixed md:relative  text-black w-64 h-screen p-5 transition-transform duration-300 ${isToggled ? "translate-x-0" : "-translate-x-64 md:translate-x-0"
+            }`}
+        >
+          <button
+            className="md:hidden text-white mb-4 ml-3"
+            onClick={() => setIsToggled(false)}
+          >
+            <X size={27} />
+          </button>
+          <ul className="flex flex-col space-y-6">
+            <li>
+              <NavLink to="#" className="hover:text-gray-300">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="hover:text-gray-300">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="hover:text-gray-300">
+                Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="#" className="hover:text-gray-300">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1 p-5">
+          {/* Toggle Button */}
+          <button
+            className="md:hidden text-black mb-4"
+            onClick={() => setIsToggled(true)}
+          >
+            <Menu size={28} />
+          </button>
+          <p>This is the main content</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
